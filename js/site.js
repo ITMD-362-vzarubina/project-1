21bc4a6bd0104b25249fe586ec0e482fbb062ee1
vzarubina <vzarubina@hawk.iit.edu>
2020-02-26 19:58:57 -0600

    js used to check that required fields are filled in

--- js/site.js
+++ js/site.js
@@ -1,3 +1,20 @@
-function confirmation() {
-    window.location.href = "confirmation.html";
+//javascript is used to make sure that the username and
+//password are filled out before form submition
+
+function validateForm() {
+  var username = document.forms["form1"]["username"].value;
+  var password = document.forms["form1"]["password"].value;
+  if (username == "" && password == "") {
+    alert("Username and Password must be filled out");
+    return false;
+  }
+  else if (password == "") {
+    alert("Password must be filled out");
+    return false;
+  }
+  else if (username == "" ) {
+    alert("Username must be filled out");
+    return false;
+  }
+
 }
